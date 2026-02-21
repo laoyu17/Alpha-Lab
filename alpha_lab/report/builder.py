2d5b5b85a1eefbe7d23b4f21c9ca9b4d1e75d05f
Yu Bo <90687482+laoyu17@users.noreply.github.com>
2026-02-22 01:01:06 +0900

    fix(report): label long-short chart as gross return

--- alpha_lab/report/builder.py
+++ alpha_lab/report/builder.py
@@ -78,7 +78,7 @@ class ReportBuilder:
             self._plot_series(
                 result.long_short_returns,
                 ls_plot,
-                f"{factor_name} Long-Short Return",
+                f"{factor_name} Long-Short Return (Gross)",
                 "Return",
             )
             self._plot_series(
@@ -116,7 +116,7 @@ class ReportBuilder:
                 "<div class='grid'>"
                 f"<img src='{factor_name}/ic.png' alt='IC' />"
                 f"<img src='{factor_name}/rank_ic.png' alt='RankIC' />"
-                f"<img src='{factor_name}/long_short.png' alt='Long Short' />"
+                f"<img src='{factor_name}/long_short.png' alt='Long Short (Gross)' />"
                 f"<img src='{factor_name}/turnover.png' alt='Turnover' />"
                 f"<img src='{factor_name}/quantiles.png' alt='Quantiles' />"
                 "</div></section>"
